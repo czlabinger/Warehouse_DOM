7931532185a53f83ae7538bd2673ac0b9582f0ff
czlabinger <czlabinger@student.tgm.ac.at>
2024-05-28 17:18:40 +0200

    Update

--- src/main/java/at/czlabinger/warehouse_dom/WarehouseDOMController.java
+++ src/main/java/at/czlabinger/warehouse_dom/WarehouseDOMController.java
@@ -4,14 +4,11 @@ import at.czlabinger.warehouse_dom.warehouse.WarehouseSimulation;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Arrays;
 
 @RestController
 @RequiredArgsConstructor
